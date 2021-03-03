ccdbe7a784581c71659b58a73d941db3996de2ec
Justin <jstnf@users.noreply.github.com>
2021-03-03 04:39:29 -0800

    Make map width and height 100%

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.listen(PORT, HOST, () => {
 app.get('/', (req, res) => {
   res.send(`
   
-  <iframe title="COVID-19 Cases Per 100,000 People in United States" aria-label="map" id="datawrapper-chart-${chartId}" src="https://datawrapper.dwcdn.net/${chartId}/${mapCurrentVersion}/" scrolling="no" frameborder="0" style="width: 0; min-width: 75% !important; border: none;" height="369"></iframe><script type="text/javascript">!function(){"use strict";window.addEventListener("message",(function(a){if(void 0!==a.data["datawrapper-height"])for(var e in a.data["datawrapper-height"]){var t=document.getElementById("datawrapper-chart-"+e)||document.querySelector("iframe[src*='"+e+"']");t&&(t.style.height=a.data["datawrapper-height"][e]+"px")}}))}();
+  <iframe title="COVID-19 Cases Per 100,000 People in United States" aria-label="map" id="datawrapper-chart-${chartId}" src="https://datawrapper.dwcdn.net/${chartId}/${mapCurrentVersion}/" scrolling="no" frameborder="0" style="width: 0; min-width: 100% !important; border: none;" height=10%></iframe><script type="text/javascript">!function(){"use strict";window.addEventListener("message",(function(a){if(void 0!==a.data["datawrapper-height"])for(var e in a.data["datawrapper-height"]){var t=document.getElementById("datawrapper-chart-"+e)||document.querySelector("iframe[src*='"+e+"']");t&&(t.style.height=a.data["datawrapper-height"][e]+"px")}}))}();
 				</script>
   
   `);
